5294dcf2d77684e01496081b2637f65b834e521f
Ben <bbjberg91@gmail.com>
2020-04-23 12:40:54 -0400

    project done

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,30 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if(item.enhancement < 20){
+  return { ...item, enhancement: item.enhancement + 1 };
+} else {
+  return item;
+}
 }
 
 function fail(item) {
-  return { ...item };
+  switch (true){
+    case (item.enhancement < 15):
+      return {...item, durability: item.durability -5};
+      
+    case (item.enhancement > 16):
+      return {...item, enhancement: item.enhancement -1, durability: item.durability -10};
+      
+    case (item.enhancement >= 15):
+      return {...item, durability: item.durability -10};
+
+      default : return item;
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,52 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const {succeed,
+    fail,
+    repair,
+    get,} = require('./enhancer.js');
+
+    describe('./enhancer.js', function() {
+        it('should run without errors', () =>{
+            expect(true).toBe(true);
+        })
+        
+        describe('.repair()', function () {
+         it('return item durability to 100', function (){
+             const item = {name: 'sword', durability: 54, enhancement: 10};
+             const expected = {name: 'sword', durability: 100, enhancement: 10};
+             const repairedItem = repair(item);
+             expect(repairedItem).toEqual(expected)
+
+         })
+        })
+
+        describe('.succeed()', function() {
+            it('enhancment goes up by one', () =>{
+                const item = {name: 'sword', durability: 100, enhancement: 12};
+                const expected = {name: 'sword', durability: 100, enhancement: 13};
+                const enhancedItem = succeed(item);
+                expect(enhancedItem).toEqual(expected);
+            })
+        })
+
+        describe('.fail()', function(){
+            it('brings durability down by 5 if lvl is < 15', () => {
+                const item = {name: 'sword', durability: 100, enhancement: 14};
+                const expected = {name: 'sword', durability: 95, enhancement: 14};
+                const failedItem = fail(item);
+            expect(failedItem).toEqual(expected);
+            })
+            it('brings durability down by 10 if lvl is >= 15', () =>{
+                const item = {name: 'sword', durability: 100, enhancement: 16};
+                const expected = {name: 'sword', durability: 90, enhancement: 16};
+                const failedItem = fail(item); 
+                expect(failedItem).toEqual(expected);   
+            })
+            it('decreses enhancement lvl down by 1 if lvl > 16', ()=>{
+                const item = {name: 'sword', durability: 100, enhancement: 17};
+                const expected = {name: 'sword', durability: 90, enhancement: 16};
+                const failedItem = fail(item); 
+                expect(failedItem).toEqual(expected); 
+            })
+        })
+
+        describe('.get()')
+    })
